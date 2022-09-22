1e0c1ca97ff69071d465405b41cc12c291480743
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-22 03:52:16 +0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,29 @@
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AppController } from './app.controller';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { config } from './config/orm.config';
-import { AppService } from './app.service';
-import { Module } from '@nestjs/common';
+
+import { MessageModule } from './modules/message/message.module';
+import { RoomModule } from './modules/room/room.module';
+import { UserModule } from './modules/user/user.module';
+import { AuthCheck } from './middleware/token';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(config),
+    RoomModule,
+    MessageModule,
+    UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    
+    consumer.apply(AuthCheck)
+    .exclude({path: "regist", method: RequestMethod.POST})
+    .forRoutes({path: "*", method: RequestMethod.ALL})
+  }
+
+}
