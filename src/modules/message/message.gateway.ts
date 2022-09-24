a1261662750a94916bbc03997870f7d6400e5854
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-24 19:10:32 +0500

    update message module

--- src/modules/message/message.gateway.ts
+++ src/modules/message/message.gateway.ts
@@ -1,21 +1,33 @@
 import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { IPayload } from "src/interface/interface";
-import { JwtService } from '@nestjs/jwt';
+import { JwtService } from "@nestjs/jwt";
 
 @WebSocketGateway()
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
+
     @WebSocketServer()
     server;
 
-    // constructor(private jwt: JwtService){}
+    constructor(private jwt: JwtService) { }
 
-    // private async checkToken (client) {
-    //     const user: IPayload = await this.jwt.verify(client.handshake.query.token);
-    // }
+    private async check(client) {
+        try {
+            return await this.jwt.verify(client.handshake.query.token)
+        } catch {
 
-    handleConnection(client){
-        console.log(client)
+            client.emit('jwt error', 'please try again, you jwt will be rejected')
+
+            return null
+        }
+    }
+
+    async handleConnection(client) {
+
+        // const user = await this.check(client)
+
+        // if (!user) return client.disconnect()
+        
         client.emit('connection', "Successfully connected to server")
     }
 
--- src/modules/message/message.module.ts
+++ src/modules/message/message.module.ts
@@ -1,8 +1,14 @@
 import { MessageService } from './message.service';
 import { ChatGateway } from './message.gateway';
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET
+    }),
+  ],
   providers: [MessageService, ChatGateway]
 })
-export class MessageModule {}
+export class MessageModule { }
