04a091bf4630be9326fe97abd44cd38998754276
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-27 05:02:09 +0300

    options update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const options = {
-    origin: "https://taskbravedeveloper.herokuapp.com/",
+    origin: "https://bravedevelopersback.herokuapp.com/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     preflightContinue: false,
     optionsSuccessStatus: 204,
