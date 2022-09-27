28ef337541171140062ca3549a5bb9a8f8d4a3e3
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-27 06:41:12 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,6 @@ require('dotenv').config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const options = {
-    origin: process.env.SERVER_CORS,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true
-  };
-  app.enableCors(options);
   app.use(cookieParser())
   await app.listen(3000);
 }
